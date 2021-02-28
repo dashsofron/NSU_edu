e9614239073ad34d974b84ef4d8da4bbe3a8fcde
Daria Sofronova <darya.sofronova20@gmail.com>
2021-02-28 10:39:21 +0700

    Delete automata theory directory

--- automata theory/src/main/java/Main.java
+++
@@ -1,8 +0,0 @@
-import java.util.List;
-public class Main {
-    public static void main(String[] args) {
-        String regularExpression = args[0];
-        ParseRegularExpression parser = new ParseRegularExpression();
-        List<StateNode> machineState=parser.parseRegularExpression(regularExpression);
-    }
-}
